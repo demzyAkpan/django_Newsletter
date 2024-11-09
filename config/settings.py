7f13ac72a3f676aa13adb47b6ea4883aed7eb7b9
demzyAkpan <demzyukonta@gmail.com>
2024-11-09 17:07:46 +0100

    modified script

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'newsletter.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'django-newsletter-v1m8.onrender.com']
 
 
 # Application definition
